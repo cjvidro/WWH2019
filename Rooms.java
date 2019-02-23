c335ea2b499cb41be35d215bffed64be78de704b
Pablo926 <47906655+Pablo926@users.noreply.github.com>
2019-02-22 22:05:01 -0500

    Room and Door
    Added more methods and added comments

--- Rooms.java
+++ Rooms.java
@@ -1,22 +1,62 @@
 
 public class Rooms {
 	
+	/*
+	 * Inner class that stores all of the info on the door.
+	 */
 	public class Door
 	{
 		public boolean isEnterable;
 		public boolean locked;
 		
+		/*
+		 * Gives the location of the door object.
+		 */
 		public void location()
 		{
 			return;
 		}
 		
+		/*
+		 * Gives the texture of the door object.
+		 */
 		public void texture()
 		{
 			return;
 		}
-		
-		
 	}
-
+	
+	/*
+	 * Gives the location of the room object.
+	 */
+	public void location()
+	{
+		return;
+	}
+	
+	/*
+	 * Gives the texture of the room object.
+	 */
+	public void texture()
+	{
+		return;
+	}
+	
+	/*
+	 * Gives the width of the room.
+	 * (Presumably number of pixels)
+	 */
+	public int width()
+	{
+		return 0;
+	}
+	
+	/*
+	 * Gives the height of the room.
+	 * (Presumably number of pixels)
+	 */
+	public int height()
+	{
+		return 0;
+	}
 }
